efae654899b3b1601b0d7a6ef4414bd99cd1d936
[kira137] <kovarzhikova@gmail.com>
2024-07-18 09:07:57 +0300

    Calculate

--- calculator16/Calculator.java
+++ calculator16/Calculator.java
@@ -2,7 +2,7 @@ package calculator16;
 
 import homework16.SimpleCalculator;
 
-public class Calculator {
+public class Calculator  {
     public static void main(String[] args) {
         SimpleCalculator calculator = new SimpleCalculator();
         double a = 6, b = 2, c = 3;
